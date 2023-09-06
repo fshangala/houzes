25e93e5ca2a661f69a104808137ee075e5854823
fshangala <fshangala@gmail.com>
2023-09-06 03:49:25 +0200

    updated the buy page using custom and plans

--- wp-content/plugins/houzez-theme-functionality/shortcodes/buy-points.php
+++ wp-content/plugins/houzez-theme-functionality/shortcodes/buy-points.php
@@ -1,12 +1,25 @@
 <?php
 if(!function_exists('houzez_render_plans')) {
-  function houzez_render_plans($gateway,$plans) {
+  function houzez_render_plans($gateway,$plans,$customPoints) {
+    ?>
+    <p class="m-4">
+      <?php
+      if($gateway == 'stripe'){
+        echo do_shortcode( '[mycred_stripe_buy amount='.$customPoints.']Buy '.$customPoints.'[/mycred_stripe_buy]');
+      } else {
+        echo do_shortcode( '[mycred_buy gateway="'.$gateway.'" amount='.$customPoints.']Buy '.$customPoints.'[/mycred_buy]');
+      }
+      ?>
+    </p>
+    <hr>
+    <p>
+    <?php
     foreach ($plans as $plan) {
       ?>
       <span class="p-2">
       <?php
       if($gateway == 'stripe'){
-        echo do_shortcode( '[mycred_stripe_buy amount='.$plan[0].']'.$plan[1].'[/mycred_buy]');
+        echo do_shortcode( '[mycred_stripe_buy amount='.$plan[0].']'.$plan[1].'[/mycred_stripe_buy]');
       } else {
         echo do_shortcode( '[mycred_buy gateway="'.$gateway.'" amount='.$plan[0].']'.$plan[1].'[/mycred_buy]');
       }
@@ -14,10 +27,13 @@ if(!function_exists('houzez_render_plans')) {
       </span>
       <?php
     }
+    ?>
+    </p>
+    <?php
   }
 }
 if(!function_exists('houzez_mycred_buy_per_gateway')){
-  function houzez_mycred_buy_per_gateway(string $gateway) {
+  function houzez_mycred_buy_per_gateway(string $gateway, string $customPoints) {
     $current_user = wp_get_current_user();
     if($current_user->roles[0] == 'houzez_seller'){
       $plans = [
@@ -27,7 +43,7 @@ if(!function_exists('houzez_mycred_buy_per_gateway')){
         ['10000','10000'],
         ['20000','20000']
       ];
-      houzez_render_plans($gateway,$plans);
+      houzez_render_plans($gateway,$plans,$customPoints);
     } elseif ($current_user->roles[0] == 'houzez_agent') {
       $plans = [
         ['1000','1000 + 100 FREE'],
@@ -36,31 +52,32 @@ if(!function_exists('houzez_mycred_buy_per_gateway')){
         ['10000','10000 + 1600 FREE'],
         ['20000','20000 + 3500 FREE']
       ];
-      houzez_render_plans($gateway,$plans);
+      houzez_render_plans($gateway,$plans,$customPoints);
+    } else {
+      echo "User is neither seller nor agent. No button could be generated!";
     }
   }
 }
 if(!function_exists('houzez_buy_credpoints')) {
   function houzez_buy_credpoints($atts) {
-    echo do_shortcode( '[mycred_buy_form]');
-    echo '<br/>';
     ?>
     <label>Payment Gateway</label>
-    <select class="form-control" id="select-gateway" value="<?php echo $_GET['gateway'] ?? ''; ?>">
-      <option value="no-gateway">No Gateway</option>
-      <option value="stripe">Stripe</option>
-      <option value="bank">Bank Transfer</option>
+    <select class="form-control" id="select-gateway" selected="<?php echo $_GET['gateway']; ?>">
+      <option value="bank" <?php if(array_key_exists('gateway',$_GET) && $_GET['gateway']=="bank"){echo "selected";}?>>Bank Transfer</option>
+      <option value="stripe" <?php if(array_key_exists('gateway',$_GET) && $_GET['gateway']=="stripe"){echo "selected";}?>>Stripe</option>
     </select><br>
+    <input class="form-control" id="points-to-buy" type="number" value=100><br>
+    <button type="button" onclick="generateButton();" class="btn btn-primary">Generate Payment Buttons</button><br>
     <script>
-      document.querySelector('#select-gateway').addEventListener('change',()=>{
-        if (event.value !== 'no-gateway') {
-          window.location.assign("?gateway="+document.querySelector('#select-gateway').value);
-        }
-      });
+      function generateButton(){
+        window.location.assign("?gateway="+document.querySelector('#select-gateway').value+"&points="+document.querySelector('#points-to-buy').value);
+      }
     </script>
     <?php
-    if(array_key_exists('gateway',$_GET)) {
-      houzez_mycred_buy_per_gateway($_GET['gateway']);
+    if(array_key_exists('gateway',$_GET) && array_key_exists('points',$_GET)) {
+      houzez_mycred_buy_per_gateway($_GET['gateway'],$_GET['points']);
+    } else {
+      houzez_mycred_buy_per_gateway('bank',"100");
     }
   }
   add_shortcode( 'houzez-buy-credpoints', 'houzez_buy_credpoints');
